903556124edb177d8aa193c5d0c1a995bb4f6471
tn9th8 <tn9th8@gmail.com>
2024-06-06 12:23:25 +0700

    add filtering user

--- src/components/UserModal.js
+++ src/components/UserModal.js
@@ -1,6 +1,15 @@
-import React, { useState } from 'react';
-import { Modal, Button, Form, Input, InputNumber, Select, message, Space } from 'antd';
-import useModal from '../customize/useModal';
+import React, { useState } from "react";
+import {
+  Modal,
+  Button,
+  Form,
+  Input,
+  InputNumber,
+  Select,
+  message,
+  Space,
+} from "antd";
+import useModal from "../customize/useModal";
 
 const { Option } = Select;
 
@@ -10,190 +19,202 @@ const layout = {
 };
 
 const tailLayout = {
-    wrapperCol: { offset: 6, span: 16 },
+  wrapperCol: { offset: 6, span: 16 },
 };
 
 const ModalUser = () => {
-    const [open, handle] = useModal();
-    const [form] = Form.useForm();
-    const [user, setUser] = useState({ 
-        firstName:'', 
-        lastName: '',
-        age: 0,
-        role: [''],
-        address: ''
+  const [open, handle] = useModal();
+  const [form] = Form.useForm();
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    age: 0,
+    role: [""],
+    address: "",
+  });
+
+  // handle on change inputs
+  const handleFirstNameChange = (value) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      firstName: value,
+    }));
+  };
+
+  const handleLastNameChange = (value) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      lastName: value,
+    }));
+  };
+
+  const handleAgeChange = (e) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      age: e,
+    }));
+  };
+
+  const handleRoleChange = (e) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      role: [e],
+    }));
+  };
+
+  const handleAddressChange = (e) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      address: e.target.value,
+    }));
+  };
+
+  // handle add button
+  const showModal = () => {
+    handle.open();
+  };
+
+  // handle form
+  const handleFinish = (e) => {
+    // to do
+    // onSubmit(user);
+    handleReset();
+    handle.close();
+  };
+
+  const handleCancel = () => {
+    handleReset();
+    handle.close();
+    message.error("No user was added");
+  };
+
+  const handleReset = () => {
+    setUser({
+      firstName: "",
+      lastName: "",
+      age: 0,
+      role: [""],
+      address: "",
     });
+    form.resetFields();
+  };
 
-    // handle on change inputs
-    const handleFirstNameChange = (value) => {
-        setUser((prevUser) => ({
-            ...prevUser,
-            firstName: value
-        }));
+  const handleFill = () => {
+    const fillUser = {
+      firstName: "Trung Nhan",
+      lastName: "Nguyen",
+      age: 20,
+      role: ["Developer"],
+      address: "Thu Duc, HCM, VN",
     };
+    form.setFieldsValue(fillUser);
+    setUser(fillUser);
+  };
 
-    const handleLastNameChange = (value) => {
-        setUser((prevUser) => ({
-            ...prevUser,
-            lastName: value
-        }));
-    };
-
-    const handleAgeChange = e => {
-        setUser((prevUser) => ({
-            ...prevUser,
-            age: e
-        }));
-    }
-
-    const handleRoleChange = e => {
-        setUser((prevUser) => ({
-            ...prevUser,
-            role: [e]
-        }));
-    }
-
-    const handleAddressChange = e => {
-        setUser((prevUser) => ({
-            ...prevUser,
-            address: e.target.value
-        }));
-    }
-
-    // handle add button
-    const showModal = () => {
-        handle.open();
-    };
-
-    // handle form
-    const handleFinish = (e) => {
-        // to do
-        // onSubmit(user);
-        handleReset();
-        handle.close();
-    };
-
-    const handleCancel = () => {
-        handleReset();
-        handle.close();
-        message.error('No user was added');
-    };
-  
-    const handleReset = () => {
-        setUser({ 
-            firstName:'', 
-            lastName: '',
-            age: 0,
-            role: [''],
-            address: ''
-        });
-        form.resetFields();
-    };
-  
-    const handleFill = () => {
-        const fillUser = { 
-            firstName: 'Trung Nhan', 
-            lastName: 'Nguyen', 
-            age: 20, 
-            role: ['Developer'], 
-            address: 'Thu Duc, HCM, VN' 
-        };
-        form.setFieldsValue(fillUser);
-        setUser(fillUser);
-    };
-    
-    return (
+  return (
     <div>
-        <Button onClick={ showModal } type="primary" style={{ marginBottom: 16 }}>
-          Add
-        </Button>
-        <Modal 
-            title="Create a user" 
-            open={ open } 
-            onCancel={handleCancel}
-            footer={() => {}} // set Modal footer rong
+      <Button onClick={showModal} type="primary" style={{ marginBottom: 16 }}>
+        Add
+      </Button>
+      <Modal
+        title="Create a user"
+        open={open}
+        onCancel={handleCancel}
+        footer={() => {}} // set Modal footer rong
+      >
+        <Form
+          {...layout}
+          form={form}
+          name="control-hooks"
+          onFinish={handleFinish}
+          style={{ maxWidth: 600 }}
         >
-            <Form
-                {...layout}
-                form={form}
-                name="control-hooks"
-                onFinish={handleFinish}
-                style={{ maxWidth: 600 }}
+          <Form.Item
+            name="firstName"
+            label="First name"
+            rules={[{ required: true }]}
+          >
+            <Input
+              placeholder="Fill your first name"
+              onChange={(e) => handleFirstNameChange(e.target.value)}
+              value={user.firstName}
+            />
+          </Form.Item>
+          <Form.Item
+            name="lastName"
+            label="Last name"
+            rules={[{ required: true }]}
+          >
+            <Input
+              placeholder="Fill your last name"
+              onChange={(e) => handleLastNameChange(e.target.value)}
+              value={user.lastName}
+            />
+          </Form.Item>
+          <Form.Item name="age" label="Age" rules={[{ required: true }]}>
+            <InputNumber
+              placeholder="Fill your age"
+              onChange={handleAgeChange}
+              value={user.age}
+            />
+          </Form.Item>
+          <Form.Item name="role" label="Role" rules={[{ required: true }]}>
+            <Select
+              placeholder="Select your role"
+              onChange={handleRoleChange}
+              value={user.role}
+              allowClear
             >
-                <Form.Item name="firstName" label="First name" rules={[{ required: true }]}>
-                    <Input 
-                        placeholder="Fill your first name"
-                        onChange={ (e) => handleFirstNameChange(e.target.value) } 
-                        value={ user.firstName }
-                    />
-                </Form.Item>
-                <Form.Item name="lastName" label="Last name" rules={[{ required: true }]}>
-                    <Input 
-                        placeholder="Fill your last name"
-                        onChange={ (e) => handleLastNameChange(e.target.value) } 
-                        value={ user.lastName }
-                    />
-                </Form.Item>
-                <Form.Item name="age" label="Age" rules={[{ required: true }]}>
-                    <InputNumber 
-                        placeholder="Fill your age"
-                        onChange={ handleAgeChange } 
-                        value={ user.age }
-                    />
-                </Form.Item>
-                <Form.Item name="role" label="Role" rules={[{ required: true }]}>
-                    <Select
-                        placeholder="Select your role"
-                        onChange={ handleRoleChange } 
-                        value={ user.role }
-                        allowClear
-                    >
-                        <Option value="Project Manager">Project Manager</Option>
-                        <Option value="Business Analyst">Business Analyst</Option>
-                        <Option value="Developer">Developer</Option>
-                        <Option value="QA/QC">QA/QC</Option>
-                        <Option value="Other">Other</Option>
-                    </Select>
-                </Form.Item>
+              <Option value="Project Manager">Project Manager</Option>
+              <Option value="Business Analyst">Business Analyst</Option>
+              <Option value="Developer">Developer</Option>
+              <Option value="QA/QC">QA/QC</Option>
+              <Option value="Other">Other</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            noStyle
+            shouldUpdate={(prevValues, currentValues) =>
+              prevValues.role !== currentValues.role
+            }
+          >
+            {({ getFieldValue }) =>
+              getFieldValue("role") === "Other" ? (
                 <Form.Item
-                    noStyle
-                    shouldUpdate={(prevValues, currentValues) => prevValues.role !== currentValues.role}
+                  name="customizeRole"
+                  label="Customize Role"
+                  rules={[{ required: true }]}
                 >
-                    {({ getFieldValue }) =>
-                    getFieldValue('role') === 'Other' ? (
-                        <Form.Item name="customizeRole" label="Customize Role" rules={[{ required: true }]}>
-                            <Input />
-                        </Form.Item>
-                    ) : null
-                    }
+                  <Input />
                 </Form.Item>
-                <Form.Item name="address" label="Address" rules={[{ required: true }]}>
-                    <Input 
-                        placeholder="Fill your address"
-                        onChange={ handleAddressChange } 
-                        value={ user.address }
-                    />
-                </Form.Item>
-                <Form.Item {...tailLayout}>
-                    <Space>
-                        <Button onClick={handleFill}>
-                            Fill form
-                        </Button>
-                        <Button onClick={handleReset}>
-                            Reset
-                        </Button>
-                        <Button type="primary" htmlType="submit">
-                            Submit
-                        </Button>
-                        <Button onClick={handleCancel}>
-                            Cancel
-                        </Button>
-                    </Space>
-                </Form.Item>
-            </Form>
-        </Modal>
+              ) : null
+            }
+          </Form.Item>
+          <Form.Item
+            name="address"
+            label="Address"
+            rules={[{ required: true }]}
+          >
+            <Input
+              placeholder="Fill your address"
+              onChange={handleAddressChange}
+              value={user.address}
+            />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Space>
+              <Button onClick={handleFill}>Fill form</Button>
+              <Button onClick={handleReset}>Reset</Button>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+              <Button onClick={handleCancel}>Cancel</Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
-    );
+  );
 };
 
 export default ModalUser;
--- src/customize/useListPage.js
+++ src/customize/useListPage.js
@@ -1,71 +1,95 @@
-import { Button, Flex, Popconfirm, message } from 'antd';
+import { Button, Flex, Popconfirm, message } from "antd";
 import axios from "axios";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const onCancel = () => {
-    message.error('No user was deleted');
+  message.error("No user was deleted");
 };
 
 const onDelete = (id) => {
-    // to do
-    message.success(`The id-${id} user was deleted`);
+  // to do
+  message.success(`The id-${id} user was deleted`);
 };
 
-function useListPage({apiObject, page}) {
-    const [data, setData] = useState([]);
-    const [pagination, setPagination] = useState({limit: 5, offset: 0, total: 0});
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
-    
-    useEffect(() => {
-        setLoading(true);
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(apiObject.getList.baseURL, {
-                    params: {
-                        offset: (page - 1) * pagination.limit,
-                        limit: pagination.limit
-                    }
-                });
-                const { data, limit, offset, total } = response.data
-                setData(data); 
-                setPagination({ 
-                    limit: +limit, 
-                    offset: +offset, 
-                    total 
-                })
-                setLoading(false);
-            } catch (error) {
-                message.error('An error occurred while attempting to call api fetching user');
-                setLoading(false);
-            }
-        };
+function useListPage({ apiObject, page }) {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState({});
+  const [pagination, setPagination] = useState({
+    limit: 5,
+    offset: 0,
+    total: 0,
+  });
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-        fetchData();
-    }, [apiObject.getList.baseURL, page, pagination.limit]);
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(apiObject.getList.baseURL, {
+          params: {
+            offset: (page - 1) * pagination.limit,
+            limit: pagination.limit,
+            ...filter,
+          },
+        });
+        const { data, limit, offset, total } = response.data;
+        setData(data);
+        setPagination({
+          limit: +limit,
+          offset: +offset,
+          total,
+        });
+        setLoading(false);
+      } catch (error) {
+        message.error(
+          "An error occurred while attempting to call api fetching user"
+        );
+        setLoading(false);
+      }
+    };
 
-    const renderAction = (_, { id }) => (
-        <Flex wrap gap="small">
-            <Button type="primary" onClick={ () => navigate(`${apiObject.getList.baseURL}/form/${id}`) } style={{ background: "Orange", borderColor: "Orange" }}>
-                Update
-            </Button>
-            <Popconfirm
-                    title="Sure to delete?"
-                    description="Are you sure to delete this user?"
-                    onConfirm={ () => onDelete(id) }
-                    onCancel={ onCancel }
-                    okText="Yes"
-                    cancelText="No"
-                >
-                <Button type="primary" danger>
-                    Delete
-                </Button>
-            </Popconfirm>
-        </Flex>
-    );
+    fetchData();
+  }, [apiObject.getList.baseURL, page, pagination.limit, filter]);
 
-    return { data, pagination, loading, renderAction };
+  const renderAction = (_, { id }) => (
+    <Flex wrap gap="small">
+      <Button
+        type="primary"
+        onClick={() => navigate(`${apiObject.getList.baseURL}/form/${id}`)}
+        style={{ background: "Orange", borderColor: "Orange" }}
+      >
+        Update
+      </Button>
+      <Popconfirm
+        title="Sure to delete?"
+        description="Are you sure to delete this user?"
+        onConfirm={() => onDelete(id)}
+        onCancel={onCancel}
+        okText="Yes"
+        cancelText="No"
+      >
+        <Button type="primary" danger>
+          Delete
+        </Button>
+      </Popconfirm>
+    </Flex>
+  );
+
+  const handleFilter = {
+    user: (user) => {
+      setFilter({
+        // fistName: user?.fistName && user.fistName !== "" : user.firstName,
+        firstName:
+          user?.firstName && user.firstName !== "" ? user.firstName : null,
+        lastName: user?.lastName && user.lastName !== "" ? user.lastName : null,
+        gender: user?.gender,
+      });
+    },
+  };
+
+  return { data, pagination, loading, renderAction, handleFilter };
 }
 
 export default useListPage;
